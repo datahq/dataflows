e3e2941566f24e82afc536c8d02f2611b6e82fb0
Adam Kariv <adam.kariv@gmail.com>
2018-10-07 19:25:52 +0300

    Don't copy resources when adding metadata

--- dataflows/processors/add_metadata.py
+++ dataflows/processors/add_metadata.py
@@ -1,4 +1,11 @@
+import copy
+
+
 def add_metadata(**metadata):
+
+    metadata = copy.deepcopy(metadata)
+    if 'resources' in metadata:
+        del metadata['resources']
 
     def func(package):
         package.pkg.descriptor.update(metadata)
