ab66d35ae0346cf436cae804cc53fec29ba23a14
Adam Kariv <akariv@users.noreply.github.com>
2018-10-15 16:19:34 +0300

    Update duplicate.py

--- dataflows/processors/duplicate.py
+++ dataflows/processors/duplicate.py
@@ -6,7 +6,7 @@ def saver(resource, db, batch_size):
     gen = db.insert_generator(
         (("{:08x}".format(idx), row)
          for idx, row
-         in enumerate(resource)), 
+         in enumerate(resource)),
         batch_size=batch_size
     )
     for _, row in gen:
