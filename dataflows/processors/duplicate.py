3c7dc356f7ad79ae55717966337e37575e28adc5
Adam Kariv <akariv@users.noreply.github.com>
2018-10-15 14:56:17 +0300

    Update duplicate.py

--- dataflows/processors/duplicate.py
+++ dataflows/processors/duplicate.py
@@ -3,7 +3,13 @@ from kvfile import KVFile
 
 
 def saver(resource, db, batch_size):
-    for _, row in db.insert_generator((("{:08x}".format(idx), row) for idx, row in enumerate(resource)), batch_size=batch_size):
+    gen = db.insert_generator(
+        (("{:08x}".format(idx), row)
+         for idx, row
+         in enumerate(resource)), 
+        batch_size=batch_size
+    )
+    for _, row in gen:
         yield row
 
 
