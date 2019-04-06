d25aec6f6df452a838c66cb884d9897201bb822f
Adam Kariv <akariv@users.noreply.github.com>
2019-04-06 08:51:13 +0300

    Improvements to add_computed_fields, allow custom functions as data source (#78)
    * Add custom functions to add_computed_field (+ some better interface and beter documentation)
    
    * Rewrite add_field with add_computed_field, allow custom callables
    
    * Fix tests

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -48,6 +48,31 @@ def test_add_computed_field():
     assert f == ['a - 1', 'b - 2', 'c - 3']
 
 
+def test_add_computed_field_func():
+    from dataflows import add_computed_field
+
+    data = [
+        dict(x=i) for i in range(3)
+    ]
+
+    f = Flow(
+        data,
+        add_computed_field([
+            dict(target=dict(name='sq', type='integer'),
+                 operation=lambda row: row['x'] ** 2),
+            dict(target='f', operation='format', with_='{x} - {x}')
+        ])
+    )
+    results, *_ = f.results()
+    results = list(results[0])
+
+    assert results == [
+        dict(x=0, sq=0, f='0 - 0'),
+        dict(x=1, sq=1, f='1 - 1'),
+        dict(x=2, sq=4, f='2 - 2'),
+    ]
+
+
 def test_add_metadata():
     from dataflows import add_metadata
     f = Flow(
