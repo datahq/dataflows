eb217b79b20f2992c840f9aff6c70a0b31344cb6
roll <eskarev@gmail.com>
2019-07-05 16:54:35 +0300

    Fixed tests' output destinations

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,5 @@
 import subprocess
 
 def test_init_remote():
-    subprocess.check_output('dataflows init https://raw.githubusercontent.com/datahq/dataflows/master/data/academy.csv',
+    subprocess.check_output('cd ./out && dataflows init https://raw.githubusercontent.com/datahq/dataflows/master/data/academy.csv',
                             shell=True)
