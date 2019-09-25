b72ff0a7c14d6037773791d14cca93e6b5644215
roll <eskarev@gmail.com>
2019-09-25 09:08:21 +0300

    Drop datetime stringification because it'ok by default

--- dataflows/processors/sort_rows.py
+++ dataflows/processors/sort_rows.py
@@ -12,17 +12,13 @@ class KeyCalc(object):
     def __call__(self, row):
         context = row.copy()
         for key, value in row.items():
-            # We need to stringify some values to make them properly comparable
+            # We need to stringify some types to make them properly comparable
             if key in self.key_list:
                 # numbers
                 # 1000 -> +1.000000e+03 -> p03ep1.000000
                 if isinstance(value, (int, float)):
                     value = 'e'.join(reversed('{:+e}'.format(value).split('e')))
                     value = value.replace('+', 'p').replace('-', 'm')
-                # date/time
-                # **** -> iso format
-                elif isinstance(value, (datetime.datetime, datetime.date, datetime.time)):
-                    value = value.isoformat()
                 context[key] = value
         return self.key_spec.format(**context)
 
