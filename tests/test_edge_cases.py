400b96f3bbaff8092f847e1eaa04ac34db42e031
Adam Kariv <akariv@users.noreply.github.com>
2021-08-23 21:39:36 +0300

    Fix full-outer join adding extraneous fields (#165)
    * Fix issue with join and differently named keys
    
    * Add test
    
    * Fix full-outer join adding extraneous fields
    
    Co-authored-by: Conrad S <conrad.schloer@gmail.com>

--- tests/test_edge_cases.py
+++ tests/test_edge_cases.py
@@ -100,3 +100,36 @@ def test_exception_information_multiple_processors_iterable_error():
     assert str(excinfo.value.cause) == 'custom-iterable-error'
     assert excinfo.value.processor_name == 'iterable_loader'
     assert excinfo.value.processor_position == 1
+
+def test_fullouter_join_dump_different_keys():
+    from dataflows import Flow, join, dump_to_path
+    from decimal import Decimal
+
+    data1 = [
+        {"col1": 1.531, "col2": "hello"},
+        {"col1": 1.132, "col2": "goodbye"},
+    ]
+    data2 = [
+        {"colA": 1.531, "colB": "123"},
+        {"colA": 1.132, "colB": 1.132},
+    ]
+    f = Flow(
+        data1,
+        data2,
+        join(
+            "res_1",
+            ["col1"],
+            "res_2",
+            ["colA"],
+            {"col2": {"name": "col2", "aggregate": "first"}},
+            mode="full-outer"
+        ),
+        dump_to_path(out_path='out/test_join_dump'),
+    )
+    results = f.results()[0][0]
+    assert results == [
+        {'colA': Decimal('1.531'), 'col2': 'hello', 'colB': '123'},
+        {'colA': Decimal('1.132'), 'col2': 'goodbye', 'colB': 1.132},
+    ]
+
+
