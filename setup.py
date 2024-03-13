3690f4e74e63577264a84325ed04fcf779d7f14b
Adam Kariv <adam.kariv@gmail.com>
2024-03-13 09:10:52 +0200

    v0.4.12

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ NAME = PACKAGE.replace('_', '-')
 INSTALL_REQUIRES = [
     'dataflows-tabulator>=1.54.0',
     'datapackage>=1.15.4',
-    'tableschema>=1.20.6',
+    'tableschema>=1.20.7',
     'kvfile>=0.0.9',
     'click',
     'jinja2',
